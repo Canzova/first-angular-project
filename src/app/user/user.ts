a2d7fbc6a2c11204b4cecdaa8f49ea83b0ccbcde
Nihal Singh <nihalsingh2082002@gmail.com>
2025-12-12 18:57:27 +0530

    InputSignal

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, input, Input } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
@@ -8,17 +8,19 @@ import { DUMMY_USERS } from './dummy-users';
   styleUrl: './user.css',
 })
 export class User {
-  // ! means that this varible will be initialized outside from this component
-  //? ---> : string shows the type of this variable
-  // {required : true} will help us in debugging. If we forgot to send it from template, file then because of this property we will see an error
-  
-  @Input({required : true}) avatar !: string;
-  @Input({required : true}) name!: string;
 
+  // @Input({required : true}) avatar !: string;
+  // @Input({required : true}) name!: string;
 
-  get imagePath() {
-    return '../../assets/users/' + this.avatar;
-  }
+  // These are signal input. Simply signals which we are getting from input method
+  // These are just read only signals. You can chnage their values.
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(()=>{
+    return '../../assets/users/' + this.avatar();
+  });
+
 
   // Methods which are ment for even binding typically starts with 'on'
   onSelectUser() {
